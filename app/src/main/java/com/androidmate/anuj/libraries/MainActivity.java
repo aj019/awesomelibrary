ba73a06d91145d2dc26bf6da63f427de36a1d18c
aj019 <junagupta19@gmail.com>
2016-07-03 15:34:40 +0530

    called toast

--- app/src/main/java/com/androidmate/anuj/libraries/MainActivity.java
+++ app/src/main/java/com/androidmate/anuj/libraries/MainActivity.java
@@ -3,11 +3,15 @@ package com.androidmate.anuj.libraries;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.androidmate.anuj.firstlibrary.FirstLibrary;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirstLibrary.getInstance().makeMeAwesome(this,"Anuj");
     }
 }
